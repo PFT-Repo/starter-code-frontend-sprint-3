ccd2628d74f7898e7d7b9413259ab899e29f59b3
santosh gauchan <santosh_gauchan@hotmail.com>
2022-11-02 11:59:59 +0100

    no level 2

--- js/checkout.js
+++ js/checkout.js
@@ -1,28 +1,102 @@
 
 // Exercise 6
 function validate() {
-	var error = 0;
+	var error = 1;
+	var errora = 1;
+	var errorb = 1;
+	var errorc = 1;
+	var errord = 1;
+	var errore = 1;
 	// Get the input fields
 	var fName = document.getElementById("fName");
+	var fLastN = document.getElementById("fLastN");
 	var fEmail = document.getElementById("fEmail");
-
-	// Get the error elements
-	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");  
+	var fAddress = document.getElementById("fAddress");
+	var fPassword = document.getElementById("fPassword");
+	var fPhone = document.getElementById("fPhone");
 	
 	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
+	var valName = /^[A-Za-z]{3,}$/;//Only letters from 3 char 
+	var valNum = /^[0-9]{3,9}$/;//Only num from 3 to 9 numbers
+	var valPass = /^[a-zA-Z0-9]{4,8}$/;//Alfanumeric from 4 to 8 char
+	var valMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // email validation format
+
+	//Results of validation True/False
+	var nameResult = valName.test(fName.value);
+	console.log(nameResult);
+	var lastNResult = valName.test(fLastN.value);
+	var emailResult = valMail.test(fEmail.value);
+	var phoneResult = valNum.test(fPhone.value);
+	var passResult = valPass.test(fPassword.value);
+	var addResult = valName.test(fAddress.value);
+
+	if(!nameResult || fName.value == " "){
+		fName.className += " is-invalid";
 		error++;
 	}
-
-	if(fEmail.value == ""){
-		error++;
+	if(!lastNResult || fLastN.value == " " ){
+		fLastN.className += " is-invalid";
+		errora++;
 	}
-	 
-	if(error>0){
-		alert("Error");
-	}else{
-		alert("OK");
+	if(!emailResult || fEmail.value == " "){
+		fEmail.className += " is-invalid";
+		errorb++;
+	}
+	if(!phoneResult || fPhone.value == " "){
+		fPhone.className += " is-invalid";
+		errorc++;
+	}
+	if(!passResult || fPassword == " "){
+		fPassword.className += " is-invalid";
+		errord++;
 	}
 
+	if(!addResult || fAddress == " " ){
+		fAddress.className += " is-invalid";
+		errore++;
+	}
+
+//If correct
+	
+	if(nameResult){
+		while(error !=0){
+			fName.classList.remove("is-invalid");
+			error--;}
+		fName.className += " is-valid";
+	}
+	if(lastNResult ){
+		while(errora !=0){
+			fLastN.classList.remove("is-invalid");
+			errora--;}
+		fLastN.className += " is-valid";
+	}
+	if(emailResult){
+		while(errorb !=0){
+			fEmail.classList.remove("is-invalid");
+			errorb--;}
+		fEmail.className += " is-valid";
+	}
+	if(phoneResult){
+		while(errorc !=0){
+			fPhone.classList.remove("is-invalid");
+			errorc--;}
+		fPhone.className += " is-valid";
+	}
+	if(passResult){
+		while(errord !=0){
+			fPassword.classList.remove("is-invalid");
+			errord--;}
+		fPassword.className += " is-valid";
+	}
+
+	if(addResult){
+		while(errore !=0){
+			fAddress.classList.remove("is-invalid");
+			errore--;}
+		fAddress.className += " is-valid";
+	}
+//Last alert
+	 if (nameResult && lastNResult && emailResult && phoneResult && passResult && addResult ) {
+		alert("Cambios Guardados Exitosamente!");
+	}
 }
--- js/shop.js
+++ js/shop.js
@@ -73,33 +73,221 @@ var total = 0;
 
 // Exercise 1
 function buy(id) {
+    var i;
+    for (let index = 0; index < products.length; index++) {
+        if(id == products[index].id){
+            i = index;
+        }
+        
+    }
+    cartList.push(products[i]);
+   console.log(cartList);
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
 }
 
 // Exercise 2
 function cleanCart() {
+  
+  cartList = [];
+  cart=[];
+  const table = document.getElementById('cart_list');
+  table.innerHTML = "Add something to cart";
+  const t_price = document.getElementById('total_price');
+  t_price.innerText = "0";
 
 }
 
 // Exercise 3
 function calculateTotal() {
+    let result=0,i=0;
+    while(i < cart.length){
+        result += cart[i].subtotalWithDiscount;
+        i++;
+    }
+    return(result);
     // Calculate total price of the cart using the "cartList" array
 }
 
 // Exercise 4
 function generateCart() {
+    if(cart[0] != undefined){
+        cart = [];
+    }
+    const checkOccurrence = (array, element) => {
+        let i=0,counter = 0,qty=0;
+        console.log("array length: "+ array.length);
+        while (i < array.length ) {
+            if (array[i].name == element) {
+                console.log("consegui una referencia")
+                counter++;
+                qty += 1;
+            }
+            i++;
+        };
+        console.log("esta es la cantidad que logré identificar de "+element+": " + qty +" y estos son los valore de I y de Counter respectivamente "+i+"/"+counter);
+        return(qty);
+    };
+
+   let coqt = checkOccurrence(cartList,'cooking oil');
+   let pqt = checkOccurrence(cartList,'Pasta');
+   let icmqt = checkOccurrence(cartList,'Instant cupcake mixture');
+   let aioqt = checkOccurrence(cartList,'All-in-one');
+   let zmqt = checkOccurrence(cartList,'Zero Make-up Kit');
+   let ltqt = checkOccurrence(cartList,'Lip Tints');
+   let ldqt = checkOccurrence(cartList,'Lawn Dress');
+   let lccqt = checkOccurrence(cartList,'Lawn-Chiffon Combo');
+   let tqt = checkOccurrence(cartList,'Toddler Frock');
+
+   if (coqt > 0) {
+    let cok = {
+        name: 'Cooking oil',
+        price: 10.5,
+        type: 'grocery',
+        cuantidad: coqt,
+        subtotal: 30,
+        subtotalWithDiscount: 30
+      }
+    cart.push(cok);
+   }
+   if (pqt > 0) {
+   let cok1 = {
+    name: 'Pasta',
+    price: 6.25,
+    type: 'grocery',
+    cuantidad: pqt,
+    subtotal: 30,
+    subtotalWithDiscount: 30
+}
+    cart.push(cok1);
+   }
+   if (icmqt > 0) {
+    let cok2 = {
+        name: 'Instant cupcake mixture',
+        price: 5,
+        type: 'grocery',
+        cuantidad: icmqt,
+        subtotal: 30,
+        subtotalWithDiscount: 30
+    }
+    cart.push(cok2);
+   }
+   if (aioqt > 0) {
+    let cok3 = {
+        name: 'All-in-one',
+        price: 260,
+        type: 'beauty',
+        cuantidad: aioqt,
+        subtotal: 30,
+        subtotalWithDiscount: 30
+    }
+    cart.push(cok3);
+   }
+   if (zmqt > 0) {
+    let cok4 = {
+        name: 'Zero Make-up Kit',
+        price: 20.5,
+        type: 'beauty',
+        cuantidad: zmqt,
+        subtotal: 30,
+        subtotalWithDiscount: 30
+}
+    cart.push(cok4);
+   }
+   if (ltqt > 0) {
+    let cok5678 = {
+        name: 'Lip Tints',
+        price: 12.75,
+        type: 'beauty',
+        cuantidad: ltqt,
+        subtotal: 30,
+        subtotalWithDiscount: 30
+    }
+    cart.push(cok5678);
+   }
+   if (ldqt > 0) {
+   let cok678 = 
+   {
+       name: 'Lawn Dress',
+       price: 15,
+       type: 'clothes',
+       cuantidad: ldqt,
+       subtotal: 30,
+       subtotalWithDiscount: 30
+   }
+    cart.push(cok678);
+   }
+   if (lccqt > 0) {
+    let cok78 = 
+    {
+        name: 'Lawn-Chiffon Combo',
+        price: 19.99,
+        type: 'clothes',
+        cuantidad: lccqt,
+        subtotal: 30,
+        subtotalWithDiscount: 30
+    }
+    cart.push(cok78);
+   }
+   if (tqt > 0) {
+    let cok8 = 
+    {
+        name: 'Toddler Frock',
+        price: 9.99,
+        type: 'clothes',
+        cuantidad: tqt,
+        subtotal: 30,
+        subtotalWithDiscount: 30
+    }
+    cart.push(cok8);
+   }
+   applyPromotionsCart();
+
     // Using the "cartlist" array that contains all the items in the shopping cart, 
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
 }
 
 // Exercise 5
 function applyPromotionsCart() {
+    const tod= (element) =>{
+            element.subtotal = element.price*element.cuantidad;
+        if (element.cuantidad >= 3) {
+            element.subtotalWithDiscount = (element.subtotal * 7) / 10;
+        }
+        else if (element.cuantidad >= 5) {
+            element.subtotalWithDiscount = (element.subtotal * 2 ) / 3;
+        }
+        else{
+            element.subtotalWithDiscount = element.subtotal;
+        }
+    }
+    cart.forEach(element => tod(element));
     // Apply promotions to each item in the array "cart"
 }
 
 // Exercise 6
 function printCart() {
+    const createelement = (element) => {
+        const table = document.getElementById('cart_list');
+        var row = table.insertRow(0);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        var nametag = new String(element.name).bold();
+        var pricetag = document.createTextNode("$"+element.price);
+        var qtytag = document.createTextNode(element.cuantidad);
+        var totaltag = document.createTextNode("$"+element.subtotalWithDiscount);
+        cell1.innerHTML=nametag;
+        cell2.appendChild(pricetag);
+        cell3.appendChild(qtytag);
+        cell4.appendChild(totaltag);
+    }
+    cart.forEach(element => createelement(element));
+    const t_price = document.getElementById('total_price');
+    res = calculateTotal();
+    t_price.innerText = res;
+    console.log(cart);
     // Fill the shopping cart modal manipulating the shopping cart dom
 }
 
@@ -121,5 +309,8 @@ function removeFromCart(id) {
 
 function open_modal(){
 	console.log("Open Modal");
+    generateCart();
+    const table = document.getElementById('cart_list');
+    table.innerHTML = "Add something to cart";
 	printCart();
 }
